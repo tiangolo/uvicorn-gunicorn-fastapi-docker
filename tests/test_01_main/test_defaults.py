6ed29d9b7db824c24e1df5f208fb21b86365e3ae
Sebastián Ramírez <tiangolo@gmail.com>
2019-03-04 18:16:02 +0400

    :white_check_mark: Update tests with default workers per core set to 1

--- tests/test_01_main/test_defaults.py
+++ tests/test_01_main/test_defaults.py
@@ -11,7 +11,7 @@ client = docker.from_env()
 
 def verify_container(container, response_text):
     config_data = get_config(container)
-    assert config_data["workers_per_core"] == 2
+    assert config_data["workers_per_core"] == 1
     assert config_data["host"] == "0.0.0.0"
     assert config_data["port"] == "80"
     assert config_data["loglevel"] == "info"
