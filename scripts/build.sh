692cbc0304b6317f7c59715943ab5c35f8af9c1c
Sebastián Ramírez <tiangolo@gmail.com>
2021-09-17 14:24:29 +0200

    🔧 Update latest tag in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ use_tag="tiangolo/uvicorn-gunicorn-fastapi:$NAME"
 DOCKERFILE="$NAME"
 
 if [ "$NAME" == "latest" ] ; then
-    DOCKERFILE="python3.8"
+    DOCKERFILE="python3.9"
 fi
 
 docker build -t "$use_tag" --file "./docker-images/${DOCKERFILE}.dockerfile" "./docker-images/"
