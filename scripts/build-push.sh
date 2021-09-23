ee8239a7d1b6eb6071da84d4be1e5b88e0a161a7
Robert Hafner <tedivm@tedivm.com>
2021-09-23 12:50:01 -0500

    Add multiarchitecture support

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -2,14 +2,30 @@
 
 set -e
 
+DOCKERFILE="$NAME"
+
+if [ "$NAME" == "latest" ] ; then
+    DOCKERFILE="python3.9"
+fi
+
+# Slim images are missing the build tools for uvloop
+if [[ "$NAME" =~ "slim" ]] ; then
+    PLATFORMS="linux/amd64"
+else
+    PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
+fi
+
 use_tag="tiangolo/uvicorn-gunicorn-fastapi:$NAME"
 use_dated_tag="${use_tag}-$(date -I)"
 
-bash scripts/build.sh
-
-docker tag "$use_tag" "$use_dated_tag"
-
 bash scripts/docker-login.sh
 
-docker push "$use_tag"
-docker push "$use_dated_tag"
+docker buildx create --use
+
+docker buildx build \
+  --platform $PLATFORMS \
+  --file "./docker-images/${DOCKERFILE}.dockerfile" \
+  -t "$use_tag"  \
+  -t "$use_dated_tag"  \
+  --push \
+  "./docker-images/"
