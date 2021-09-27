6f4bc3e016e8f89f4f06672bda6d4adb055ed2cc
Robert Hafner <tedivm@tedivm.com>
2021-09-27 10:28:26 -0500

    add slim builds back

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -8,13 +8,6 @@ if [ "$NAME" == "latest" ] ; then
     DOCKERFILE="python3.9"
 fi
 
-# Slim images are missing the build tools for uvloop
-if [[ "$NAME" =~ "slim" ]] ; then
-    PLATFORMS="linux/amd64"
-else
-    PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
-fi
-
 use_tag="tiangolo/uvicorn-gunicorn-fastapi:$NAME"
 use_dated_tag="${use_tag}-$(date -I)"
 
@@ -23,7 +16,7 @@ bash scripts/docker-login.sh
 docker buildx create --use
 
 docker buildx build \
-  --platform $PLATFORMS \
+  --platform "linux/amd64,linux/arm64,linux/arm/v7" \
   --file "./docker-images/${DOCKERFILE}.dockerfile" \
   -t "$use_tag"  \
   -t "$use_dated_tag"  \
