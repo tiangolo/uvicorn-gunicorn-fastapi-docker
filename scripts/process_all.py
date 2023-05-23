e0770dc5676768089a2689625c53012c90271fab
abhint <notifyabhijith@gmail.com>
2023-05-23 11:40:58 +0530

    Optimize main() function 🚀

--- scripts/process_all.py
+++ scripts/process_all.py
@@ -36,16 +36,22 @@ def print_version_envs():
     env_lines = [" ".join([f"{key}='{value}'" for key, value in env.items()]) for env in environments]
     print("\n".join(env_lines))
 
+# def main():
+#     start_at = 0
+#     if start_with:
+#         start_at = [
+#             i for i, env in enumerate((environments)) if env["NAME"] == start_with
+#         ][0]
+#     for i, env in enumerate(environments[start_at:]):
+#         print(f"Processing tag: {env['NAME']}")
+#         process_tag(env=env)
+
+
 def main():
-    start_at = 0
-    if start_with:
-        start_at = [
-            i for i, env in enumerate((environments)) if env["NAME"] == start_with
-        ][0]
-    for i, env in enumerate(environments[start_at:]):
+    start_at = next((i for i, env in enumerate(environments) if env["NAME"] == start_with), 0)
+    for env in environments[start_at:]:
         print(f"Processing tag: {env['NAME']}")
         process_tag(env=env)
-
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
